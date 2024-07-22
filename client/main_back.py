f752a77708e31b0d52369dec066d01bfa8608d10
lugamecooper <legamerfdp81@gmail.com>
2024-07-22 20:28:45 +0200

    add some stuffs

--- client/main_back.py
+++ client/main_back.py
@@ -1,6 +1,56 @@
 import socket
 import _thread
+import json
+import os
+import os.path
+from psutil import net_if_addrs
+from re import findall
+import ipaddress
 
 class back_end:
     def __init__(self) -> None:
-        pass
+        #get the configuration
+        self.json = open(os.path.join(os.path.split(__file__)[0],"config_back.json"))
+        self.config = json.load(self.json)
+
+        #define default boolean
+        self.validate_deafault_ip = False
+
+    def get_all_ip(self):
+        interfaces = net_if_addrs()
+        data = []
+        network = {}
+        for interface_name, interface_addresses in interfaces.items():
+            for address in interface_addresses:
+                if address.family == socket.AF_INET and address.netmask:
+                    data.append(address.address)
+                    network[address.address] = address.netmask
+                elif address.family == socket.AF_INET6 and address.netmask:
+                    data.append(address.address)
+                    network[address.address] = address.netmask
+        # delete the last element because it's allways "::1"
+        data.pop()
+        #return the data to be display and the network to have the netmask
+        return data,network
+    
+    def check_ip_and_mask(self,ip = str(),mask = str()):
+        ip = ip.split(".")
+        mask = mask.split(".")
+        if not len(ip) == 4 and len(mask) == 4:
+            self.validate_deafault_ip = ["#00#","l'adresse ou le masque fournie n'est pas correcte"]
+            return None
+        result = []
+        for index in range(4):
+            try:
+                ip_split    = bool(int(ip[index]))
+                mask_split  = bool(int(mask[index]))
+                result_split = ""
+                for null in range(8-len(ip_split)):
+                    ip_split += "0"
+                for null in range(8-len(mask_split)):
+                    mask_split += "0"
+                for null in range(8):
+                    pass #------------------------------------------------------------
+            except:
+                self.validate_deafault_ip = ["#00#","l'adresse ou le masque fournie n'est pas correcte"]
+                return None
--- client/main_interface.py
+++ client/main_interface.py
@@ -1,8 +1,59 @@
 from main_back import back_end
-import customtkinter
+import customtkinter as tk
+import _thread
+import socket
+import psutil
 
 class main:
     def __init__(self) -> None:
-        pass
+        self.back = back_end()
+        self.fen = tk.CTk()
 
+        # variable use to be check boolean
+        self.first_use_test_bool_1 = True
+
+
+        self.fen.title("messagerie canardessque")
+        self.fen._state_before_windows_set_titlebar_color = 'zoomed'
+        if self.back.config["first use"]:
+            self.first_use()
+
+        self.fen.mainloop()
+
+    def custom_ip(self):
+        dico_state = {True:["normal","readonly"],False:["disabled","disabled"]}
+        self.first_use_test_bool_1 = not self.first_use_test_bool_1
+        self.dropdown_menu_first_use.configure(state=dico_state[self.first_use_test_bool_1][1])
+        self.entry_netmask_first_use.configure(state=dico_state[not self.first_use_test_bool_1][0], placeholder_text="entrer le masque réseaux",)
+        self.entry_ip_first_use.configure(state=dico_state[not self.first_use_test_bool_1][0], placeholder_text="entrer l'adresse ip")
+
+    def first_use(self):
+        self.data, self.netmask_and_data = self.back.get_all_ip()
+        self.frame_first_use = tk.CTkFrame(self.fen, width=200, height=200)
+        
+        self.checkbutton_1_first_use = tk.CTkCheckBox(self.frame_first_use,text="rentrer sa propre adresse ip et masque de sous réseaux",command=self.custom_ip)
+        self.dropdown_menu_first_use = tk.CTkComboBox(self.frame_first_use, values=self.data, state="readonly")
+        self.dropdown_menu_first_use.set(self.data[0])
+        self.entry_ip_first_use = tk.CTkEntry(self.frame_first_use,placeholder_text="entrer l'adresse ip", state="disable")
+        self.entry_netmask_first_use = tk.CTkEntry(self.frame_first_use,placeholder_text="entrer le masque réseaux", state="disable")
+        self.button_first_use = tk.CTkButton(self.frame_first_use,text="vallidez l'addresse ip à utiliser", command=self.get_first_use)
+        self.label_error_first_use = tk.CTkLabel(self.frame_first_use,text="")
+
+        self.frame_first_use.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+        self.checkbutton_1_first_use.grid()
+        self.dropdown_menu_first_use.grid()
+        self.entry_ip_first_use.grid()
+        self.entry_netmask_first_use.grid()
+        self.button_first_use.grid()
+        self.label_error_first_use.grid()
+
+    def get_first_use(self):
+        if self.first_use_test_bool_1:
+            self.back.check_ip_and_mask(self.dropdown_menu_first_use.get(),self.netmask_and_data[self.dropdown_menu_first_use.get()])
+            while True:
+                if self.back.validate_deafault_ip:
+                    if type(self.back.validate_deafault_ip) == type([]):
+                        if self.back.validate_deafault_ip[0] == "#00#":
+                            self.label_error_first_use.configure(text=self.back.validate_deafault_ip[1])
+                            break
 main()
