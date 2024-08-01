67cca66df3bcb64674f1aca242c0d9568696838b
lugamecooper <legamerfdp81@gmail.com>
2024-08-02 00:25:47 +0200

    .

--- client/main_back.py
+++ client/main_back.py
@@ -1,11 +1,10 @@
-import socket
+from socket import socket,AF_INET,SOCK_STREAM
 import _thread
 import json
 import os
 import os.path
 from re import findall
 from ip.main import IP
-
 class back_end:
     def __init__(self) -> None:
         #get the configuration
@@ -14,6 +13,9 @@ class back_end:
 
         #define default boolean
         self.validate_deafault_ip = False
+
+        #define default variable
+        self.message_reciev = False
     
     def check_ip(self,ip : str):
         try:
@@ -24,8 +26,24 @@ class back_end:
 
             json.dump(self.config,open(os.path.join(os.path.split(__file__)[0],"config_back.json"),"w"))
             self.json = open(os.path.join(os.path.split(__file__)[0],"config_back.json"))
-
             return True
         except Exception as er:
             print(er)
             return ["#00#",er]
+        
+    def connect_server(self):
+        self.connection_server = socket(AF_INET, SOCK_STREAM)
+        while True:
+            try:
+                self.connection_server.connect()
+                break
+            except:
+                pass
+        while True:
+            test = self.connection_server.recv(1024)
+            if test:
+                self.message_reciev = test
+
+        
+    def get_log(self):
+        pass
