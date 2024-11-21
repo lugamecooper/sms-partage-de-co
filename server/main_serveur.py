5803c2f352832cc17ceb2054d73b1db8e6d3ae56
lugamecooper <legamerfdp81@gmail.com>
2024-11-21 17:43:02 +0100

    .

--- server/main_serveur.py
+++ server/main_serveur.py
@@ -6,13 +6,15 @@ class main:
     def __init__(self) -> None:
         #initialise le serveur et lance en parralèlle la méthode start_co
         self.client_connecter = []
-        system("ip config -all")
+        if name=="nt":
+            system("ipconfig -all")
+        else:
+            system("ifconfig")
         self.ip_pc = input("qu'elle est l'ip ? ")
         if name == "nt":
             system("cls")
         else:
             system("clear")
-        del system,name
         code_session = findall(r"\d*.\d*.\d*.(\d*)",self.ip_pc)
         print(f"l'ip est {self.ip_pc}")
         print(f"le code session est {code_session}")
